20b71cc05708c92a117fec7c48d726aaa503072f
Bailey <bsym307@gmail.com>
2020-07-20 21:58:40 +0100

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,57 +1,66 @@
 import markovify
 
+
 # Get raw text as string.
 print("Reading the corpus...")
 with open("aurelius.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
+
 # Build the model.
 print("Building the model...")
 text_model = markovify.Text(text)
 
+
 # Generate a valid line
 def make_line(max_chars):
     line = text_model.make_short_sentence(max_chars)
-    if line == None:
-        return make_line(max_chars)
+    if line == None: return make_line(max_chars)  # If the line is empty, regenerate it
     return line
 
-# Show which line has been changed
-def indicator(x, y):
-    if x == y:
-        return "*"
-    return " "
 
+# Main loop
 while (True):
     choice = input("\nRandom poem (R) or user assisted (U)?: ")
 
+    # Generate a random poem with no human intervention
     if choice == "R" or choice == "r":
-        # Generate poetry
         print("\nYour poem:")
-        for line in range(6):  # 6 lines per poem
-            print("   " + make_line(30))  # under 30 characters per line
+        for line in range(6):
+            print("   " + make_line(30))
 
+    # Generate a random poem and allow the user regenerate certain lines
     elif choice == "U" or choice == "u":
-        # User assists
         lines = ["", "", "", "", "", ""]
-        # Generate a poem
-        print("\nYour poem:")
+
+        # Generate six random lines
         for i in range(6):
             line = make_line(30)
             lines[i] = line
-            print(str(i+1) + ". " + line)
+        
+        # Print the lines together
+        output = "\n"
+        for i in range(6):
+            output += str(i + 1) + "  " + lines[i]
+            if i < 5: output += "\n"
+        print(output)
 
+        # Repeat until user exits
         editing = True
         while (editing):
-            # Get user input
-            line_to_change = input("\nEnter a line to change: ")
-            if not line_to_change in ("1", "2", "3", "4", "5", "6"):
+            ltc = input("\nEnter a line to randomise: ")
+            
+            # Only input of 1-6 is valid here
+            if not ltc in ("1", "2", "3", "4", "5", "6"):
                 editing = False
                 break
-            line_to_change = int(line_to_change) - 1
-            # Change that line
-            print("\nYour new poem:")
-            lines[line_to_change] = make_line(30)
+
+            # Regenerate the chosen line
+            lines[int(ltc)-1] = make_line(30)
+
+            # Print the lines together
+            output = "\n-\n\n"
             for i in range(6):
-                ind = indicator(i, line_to_change)
-                print(str(i+1) + "." + ind + lines[i] + ind)
+                output += str(i + 1) + "  " + lines[i]
+                if i < 5: output += "\n"
+            print(output)
